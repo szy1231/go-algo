519bb07b7d9e4f07e9f3fc21ddfc750897bab5f0
pasiyu <pasiyu0814@163.com>
2020-05-16 19:29:15 +0800

    更新leetcode2

---
+++ leetcode/two_addTwoNumbers.go
@@ -0,0 +1,43 @@
+package leetcode
+
+/*
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	headList := new(ListNode)
+	head := headList
+	num := 0
+
+	for l1 != nil || l2 != nil || num > 0 {
+		headList.Next = new(ListNode)
+		headList = headList.Next
+		if l1 != nil {
+			num = num + l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			num = num + l2.Val
+			l2 = l2.Next
+		}
+		headList.Val = (num) % 10
+		num = num / 10
+	}
+
+	return head.Next
+}
