80a4522d539fdd2bb81f97488a338b3e823de17e
pasiyu <pasiyu0814@163.com>
2020-08-12 00:11:31 +0800

    力扣125题,验证回文串

---
+++ leetcode/isPalindrome.go
@@ -0,0 +1,54 @@
+package leetcode
+
+import "strings"
+
+//125. 验证回文串
+/*
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+示例 2:
+
+输入: "race a car"
+输出: false
+
+*/
+func main() {
+	isPalindrome("race a car")
+
+}
+
+//直接循环判断开头和结尾是否相同
+func isPalindrome(s string) bool {
+
+	//全部转换为小写
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+
+	for i < len(s) && j >= 0 {
+		//如果是其他字符串，直接跳过
+		if !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')) {
+			i++
+			continue
+		}
+		//如果是其他字符串，直接跳过
+		if !((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= '0' && s[j] <= '9')) {
+			j--
+			continue
+		}
+
+		if s[i] == s[j] {
+			i++
+			j--
+			continue
+		}
+		return false
+	}
+	return true
+
+}
